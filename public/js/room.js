7a0857d19d231ad110d0413f4fb59bf1e53ea135
RajatBachhawat <58325689+RajatBachhawat@users.noreply.github.com>
2021-06-29 02:24:58 +0530

    Add working Copy to Clipboard button

--- public/js/room.js
+++ public/js/room.js
@@ -1,18 +1,11 @@
 let roomURL=null;
 
+// Writes the meeting URL to the read only text field
 function setURL(){
   document.getElementById('create-meeting-url').value=roomURL;
 }
 
-function toggleVisibility(form) {
-  document.querySelector('.form-holder').style.position="static";
-  form.static
-  if(form.style.visibility="visible")
-    form.style.visibility="hidden";
-  else
-    form.style.visibility="visible";
-}
-
+// Handles entry of the join room form
 function enterJoinForm(){
   let joinForm = document.querySelector('.join-form');
   let createForm = document.querySelector('.create-form');
@@ -26,6 +19,7 @@ function enterJoinForm(){
   joinForm.classList.add('animate__animated', 'animate__fadeInLeft');
 }
 
+// Handles entry of the create room form
 function enterCreateForm(){
   let joinForm = document.querySelector('.join-form');
   let createForm = document.querySelector('.create-form');
@@ -37,8 +31,25 @@ function enterCreateForm(){
 
   createForm.classList.remove('animate__animated', 'animate__fadeOutRight');
   createForm.classList.add('animate__animated', 'animate__fadeInLeft');
-
+  
+  /* can also just pass uuidv4() */
   roomURL=window.location.href+uuidv4();
   
   setURL();
+}
+
+// Copies the meeting URL on the text field to the clipboard
+function copyURL() {
+  /* Get the text field */
+  let copyText = document.getElementById('create-meeting-url');
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
 }
